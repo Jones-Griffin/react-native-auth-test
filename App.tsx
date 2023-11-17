0ff9168ad26ff37bee172ae3f7dfd45b5390645f
Griffin Jones <jonesg873@gmail.com>
2023-11-17 11:09:19 +0800

    what

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback } from 'react';
+import { Button, Platform, StyleSheet, Text, View } from 'react-native';
+import { openLink2 } from './utils';
+
+const url = 'https://google.com'
 
 export default function App() {
+
+  const onOpenLink = useCallback(async () => {
+    await openLink2(url);
+  }, [url]);
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.openButton}>
+            <Button title="Open link" onPress={onOpenLink} />
+          </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +27,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  openButton: {
+    paddingTop: Platform.OS === 'ios' ? 0 : 20,
+    paddingBottom: Platform.OS === 'ios' ? 0 : 20,
   },
 });
